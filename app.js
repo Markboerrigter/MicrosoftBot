5da6f3a113e23c9a5d956b9a3f50761f2af011e1
Mark Romme <Mark Romme>
2016-12-27 13:34:25 +0100

    'ji'

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
   
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: '422495b1-a3b3-4e35-bf27-69484072b7dd',
+    appPassword: 'NgfJ4a6Km4Tszea3b91KCUW'
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
