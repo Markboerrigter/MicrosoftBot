db8d3b8e5786cdacd3485de7dbaf53eb0a911e55
Mark Romme <Mark Romme>
2016-12-27 13:36:22 +0100

    'ji'

--- app.js
+++ app.js
@@ -26,27 +26,3 @@ server.post('/api/messages', connector.listen());
 bot.dialog('/', function (session) {
     session.send("Hello World");
 });
-
-
-
-
-bot.dialog('/picture', [
-    function (session) {
-		var myRandomElement = images.randomElement()
-        session.send("I'm now sending you a picture, could you tell me what you think it is?");
-        var msg = new builder.Message(session)
-            .attachments([{
-                contentType: "image/jpeg",
-                contentUrl: myRandomElement
-            }]);
-        session.endDialog(msg);
-    }
-]);
-
-bot.dialog('/end', [
-    function (session, results) {
-        // Always say goodbye
-        session.send("Ok... See you later!");
-    }
-]);
-
