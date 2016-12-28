37bffb94a54ceef3821e1ecd888c47bd5134481c
Mark Romme <Mark Romme>
2016-12-28 11:51:27 +0100

    'hi'

--- app.js
+++ app.js
@@ -18,15 +18,16 @@ bot.dialog('/', function (session) {
 // Setup Restify Server
 var server = restify.createServer();
 server.use(restify.CORS());
-// Handle Bot Framework messages
-server.post('/api/messages', connector.listen());
-
-// Serve a static web page
-server.get(/.*/, restify.serveStatic({
-	'directory': '.',
-	'default': 'index.html'
-}));
-
 server.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
+// Handle Bot Framework messages
+
+/* 
+// Serve a static web page
+server.get(//, restify.serveStatic({
+	'directory': '.',
+	'default': 'index.html'
+}));
+ */
+server.post('/api/messages', connector.listen());
