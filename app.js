6c76d4700ee88293ece3565e1ec554ef9e8bc6fd
Mark Romme <Mark Romme>
2016-12-28 10:58:11 +0100

    'hi'

--- app.js
+++ app.js
@@ -1,77 +1,32 @@
 var restify = require('restify');
 var builder = require('botbuilder');
-var fs = require("fs");
 
-//=========================================================
-// Bot Setup
-//=========================================================
+// Get secrets from server environment
+var botConnectorOptions = { 
+    appId: '422495b1-a3b3-4e35-bf27-69484072b7dd',
+    appPassword: 'NgfJ4a6Km4Tszea3b91KCUW'
+};
+
+// Create bot
+var bot = new builder.BotConnectorBot(botConnectorOptions);
+bot.add('/', function (session) {
+    
+    //respond with user's message
+    session.send("You said " + session.message.text);
+});
 
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+
+// Handle Bot Framework messages
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+
+// Serve a static web page
+server.get(/.*/, restify.serveStatic({
+	'directory': '.',
+	'default': 'index.html'
+}));
+
+server.listen(process.env.port || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url); 
 });
-  
-Array.prototype.randomElement = function () {
-    return this[Math.floor(Math.random() * this.length)]
-}
-// Create chat bot
-var connector = new builder.ChatConnector({
-    appId: '422495b1-a3b3-4e35-bf27-69484072b7dd',
-    appPassword: 'NgfJ4a6Km4Tszea3b91KCUW'
-});
-var bot = new builder.UniversalBot(connector);
-var contents = fs.readFileSync("images.json");
-var jsonContent = JSON.parse(contents);
-var images = jsonContent.images;
-var myRandomElement = images.randomElement()
-server.post('/api/messages', connector.listen());
-
-//=========================================================
-// Bots Middleware
-//=========================================================
-
-// Anytime the major version is incremented any existing conversations will be restarted.
-bot.use(builder.Middleware.dialogVersion({ version: 1.0, resetCommand: /^reset/i }));
-
-//=========================================================
-// Bots Global Actions
-//=========================================================
-
-
-//=========================================================
-// Bots Dialogs
-//=========================================================
-
-bot.dialog('/', [
-    function (session) {
-        // Send a greeting and show help.
-        session.send("Hi... I'm Immy, together we will play a game!");
-        session.beginDialog('/picture');
-    }
-]);
-
-
-
-
-
-bot.dialog('/picture', [
-    function (session) {
-		var myRandomElement = images.randomElement()
-        session.send("I'm now sending you a picture, could you tell me what you think it is?");
-        var msg = new builder.Message(session)
-            .attachments([{
-                contentType: "image/jpeg",
-                contentUrl: myRandomElement
-            }]);
-        session.endDialog(msg);
-    }
-]);
-
-bot.dialog('/end', [
-    function (session, results) {
-        // Always say goodbye
-        session.send("Ok... See you later!");
-    }
-]);
-
