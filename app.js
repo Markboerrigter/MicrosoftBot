be5dc8ad76dc880d9f7124b290bd7041195839ef
Mark Romme <Mark Romme>
2016-12-28 11:04:16 +0100

    'hi'

--- app.js
+++ app.js
@@ -27,6 +27,6 @@ server.get(/.*/, restify.serveStatic({
 	'default': 'index.html'
 }));
 
-server.listen('https://imageguesser3.azurewebsites.net/', function () {
+server.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
