b73862b4b718ec1450f65daa1322f118e45395b7
Mark Romme <Mark Romme>
2016-12-28 12:03:17 +0100

    'hi'

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ var botConnectorOptions = {
     appPassword: 'NgfJ4a6Km4Tszea3b91KCUW'
 };
 // Create bot
-var bot = new builder.BotConnectorBot(botConnectorOptions);
-bot.add('/', function (session) {
-    
-    //respond with user's message
-    session.send("You said " + session.message.text);
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
-
+var bot = new builder.UniversalBot(connector);
+bot.dialog('/', function (session) {
+   session.send('Hello World'); 
+});
 // Setup Restify Server
 var server = restify.createServer();
 server.use(restify.CORS());
